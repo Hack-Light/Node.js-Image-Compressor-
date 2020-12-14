c50c1272d84e3ad65f32a6f1c9042812942edbf8
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-12-14 02:17:31 +0100

    Everthing works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let path = require("path");
 let multer = require("multer");
+let sharp = require("sharp");
 
 let app = express();
 
@@ -9,10 +10,43 @@ let storage = multer.diskStorage({
     cb(null, path.join(__dirname, "upload"));
   },
   filename: (req, file, cb) => {
-    cb(null, `${file.originalname} - ${new Date.now()}`);
+    cb(null, `${file.originalname}`);
   }
 });
 
 let upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("picture"), async (req, res) => {
+  let compressedImagePath = path.join(
+    __dirname,
+    "compressed",
+    new Date().getTime() + ".jpeg"
+  );
+  try {
+    if (req.file.mimetype == "image/jpeg") {
+      sharp(req.file.path)
+        .jpeg({ quality: 50, chromaSubsampling: "4:4:4" })
+        .toFile(compressedImagePath, (err, info) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send(info);
+          }
+        });
+    } else if (req.file.mimetype == "image/png") {
+      sharp(req.file.path)
+        .png({ quality: 50, chromaSubsampling: "4:4:4" })
+        .toFile(compressedImagePath, (err, info) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send(info);
+          }
+        });
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 app.listen(2000, () => console.log(`Server running on port 2000`));
